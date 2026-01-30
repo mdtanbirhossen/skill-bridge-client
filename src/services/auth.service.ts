bb0a98f7a8cc9359ff5a1a4df47448123906efcc
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-01-30 19:31:25 +0600

    feat: created signup , signin and logout method

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,11 @@
 import { apiFetchClient } from "@/lib/api.client";
-
-
+import { apiFetchServer } from "@/lib/api.server";
+import { JwtUserPayload } from "@/types/user.types";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 export const authService = {
+  getMe: () => apiFetchServer("/auth/me"),
+  
   signUp: (payload: {
     name: string;
     email: string;
@@ -20,10 +24,27 @@ export const authService = {
       body: JSON.stringify(payload),
     }),
 
-  getMe: () => apiFetchClient("/auth/me"),
-
   logout: () =>
     apiFetchClient("/auth/logout", {
       method: "POST",
     }),
+
+  getSession: async (): Promise<JwtUserPayload | null> => {
+    const cookieStore = await cookies();
+    const tokenCookie = cookieStore.get("token");
+
+    if (!tokenCookie) return null;
+
+    try {
+      const decoded = jwt.verify(
+        tokenCookie.value,
+        process.env.JWT_SECRET!,
+      ) as JwtUserPayload;
+
+      return decoded;
+    } catch (err) {
+      console.log("JWT invalid or expired:", err);
+      return null;
+    }
+  },
 };
