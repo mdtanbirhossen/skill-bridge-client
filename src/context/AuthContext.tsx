ec8d9c9366941689fb005627ab79b859c8a43b9b
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-01-30 23:12:56 +0600

    feat: created auth context for user state management

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,71 @@
+"use client"
+import { createContext, useContext, useState, ReactNode, useEffect } from "react";
+import Cookies from "js-cookie";
+
+//  User type
+export type User = {
+  name: string;
+  email?: string;
+};
+
+//  Context type
+type AuthContextType = {
+  user: User | null;
+  login: (userData: User, token: string) => void;
+  setUser: (user: User | null) => void;
+  logout: () => void;
+  token: string | null;
+};
+
+//  Create context
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+//  Provider props type
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+//  Provider
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  // Load user and token from cookies
+  useEffect(() => {
+    const cookieToken = Cookies.get("auth-token");
+    const cookieUser = Cookies.get("user");
+    if (cookieToken) setToken(cookieToken);
+    if (cookieUser) setUser(JSON.parse(cookieUser));
+  }, []);
+
+  const login = (userData: User, token: string) => {
+    setUser(userData);
+    setToken(token);
+
+    // Save in cookies
+    Cookies.set("auth-token", token, { expires: 7 }); // expires in 7 days
+    Cookies.set("user", JSON.stringify(userData), { expires: 7 });
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    Cookies.remove("auth-token");
+    Cookies.remove("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user,setUser, logout,login, token }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
+};
