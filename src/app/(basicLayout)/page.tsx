5df2ece25d41ed2f9723150e2f69cfc4297cee0a
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-01-30 17:37:34 +0600

    initiated register page

--- src/app/(basicLayout)/page.tsx
+++ src/app/(basicLayout)/page.tsx
@@ -1,6 +1,10 @@
 
+import { userService } from "@/services/user.service";
 
-export default function HomePage() {
+
+export default async function HomePage() {
+  const data = await userService.getSession()
+  console.log(data)
   return (
     <div className="">
       this is home page
