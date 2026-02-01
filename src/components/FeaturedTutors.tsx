065a294755ed1ac5f81cc177c3b9b3084953539e
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-02-01 19:37:44 +0600

    feat: created featured component

---
+++ src/components/FeaturedTutors.tsx
@@ -0,0 +1,18 @@
+import { tutorService } from "@/services/tutor.service";
+import { TutorCard } from "./TutorCard";
+import { Tutor } from "@/types/tutor.types";
+
+const FeaturedTutors = async () => {
+    const data = await tutorService.getAllTutors({ limit: 6 })
+    console.log(data.data.data)
+    return (
+        <div className=" py-20">
+            <h2 className="text-3xl font-bold mb-4">Featured Tutors</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+                {data?.data?.data?.map((tutor: Tutor) => <TutorCard key={tutor.id} tutor={tutor} />)}
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedTutors;
