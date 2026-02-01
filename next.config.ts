4c3667263233dbbbf4d1108ac2d5b90d77e0dabe
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-02-02 01:59:58 +0600

    feat: image bb hosting image domain added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["i.ibb.co"], // add all external domains you use for images here
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
