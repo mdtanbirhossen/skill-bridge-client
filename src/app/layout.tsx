173e38c88d771ff0a54c7ceedd2fbb14511b14b1
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-02-01 15:19:30 +0600

    feat: added space-grotesk-font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "sonner";
+import { spaceGrotesk } from "./fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${spaceGrotesk.className} font-sans `}
       >
         <Toaster/>
         <AuthProvider>{children}</AuthProvider>
